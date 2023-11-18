d653dc5bafcbf33148784bd286a7847191fc5366
joaquinrodriguezm7 <127951649+joaquinrodriguezm7@users.noreply.github.com>
2023-11-18 16:21:56 -0300

    avances validaciones

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +10,30 @@ import { Router } from '@angular/router';
 export class LoginPage implements OnInit {
 
   user: string = '';
+  forma!: FormGroup;
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.crearFormulario();
+  }
 
-  constructor(private router: Router) {}
+  get invalidUser(){
+    return this.forma.get('usuario')?.invalid && this.forma.get('usuario')?.touched; 
+  }
+
+  get invalidPass(){
+    return this.forma.get('pass')?.invalid && this.forma.get('pass')?.touched; 
+  }
+
+  crearFormulario(){
+
+    this.forma = this.fb.group({
+      usuario:['', [Validators.required, Validators.minLength(5)]],
+      pass:['', Validators.required]
+    })
+  }
+
+  guardar(){
+    console.log(this.forma);
+  }
 
   ngOnInit() {
   }
