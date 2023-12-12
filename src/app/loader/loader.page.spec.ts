8e8fad799a87fd0eb8e43e9ee0ca445fd79b4519
joaquinrodriguezm7 <127951649+joaquinrodriguezm7@users.noreply.github.com>
2023-12-12 01:17:10 -0300

    avances

--- src/app/loader/loader.page.spec.ts
+++ src/app/loader/loader.page.spec.ts
@@ -1,17 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async, fakeAsync, flush, tick } from '@angular/core/testing';
 import { LoaderPage } from './loader.page';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { LoginPage } from '../login/login.page';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
-  beforeEach(async() => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoaderPage],
+      imports: [HttpClientTestingModule,
+        RouterTestingModule.withRoutes([{ path:'login', component: LoginPage}]),
+        IonicModule.forRoot()
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoaderPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    router = TestBed.inject(Router);
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate to /login after timeout', fakeAsync(() => {
+    spyOn(router, 'navigate').and.callThrough();
+
+    component.simulateLoading();
+
+    fixture.detectChanges();
+
+    tick(750);
+
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+    flush();
+  }));
 });
